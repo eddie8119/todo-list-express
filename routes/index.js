ed3692e4ce20e0a72315b3de86be1430fd8fe9c7
eddie wang <wang8119@gmail.com>
2024-11-04 09:49:29 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const home = require("./modules/home");
 const todos = require("./modules/todos");
 const users = require("./modules/users");
 
-router.use("/", home); //將網址結構符合 / 字串的 request 導向 home 模組
-router.use("/todos", todos);
+const { authenticator } = require("../middleware/auth");
+
+router.use("/todos", authenticator, todos);
 router.use("/users", users);
+router.use("/", authenticator, home); //將網址結構符合 / 字串的 request 導向 home 模組
 
 module.exports = router;
